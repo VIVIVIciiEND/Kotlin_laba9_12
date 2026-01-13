7d773e7ce3d134dbd4d71266363ceccc01ba92b6
VIVIVIciiEND <akirmandinara@gmail.com>
2026-01-13 15:20:57 +0300

    Add data class OutpostResource and ResourceManager

---
+++ src/ResourceManager.kt
@@ -0,0 +1,16 @@
+class ResourceManager {
+    private val resources = mutableListOf<OutpostResource>()
+    fun add(resource: OutpostResource){
+        resources.add(resource)
+        println("добавлен ресурс ${resource.name}")
+    }
+    fun get(name:String):OutpostResource? {
+        return resources.find { it.name == name}
+    }
+    fun printAll(){
+        println("ресурсф базы")
+        resources.forEach { println("${it.name} ${it.amount}") }
+    }
+
+
+}
